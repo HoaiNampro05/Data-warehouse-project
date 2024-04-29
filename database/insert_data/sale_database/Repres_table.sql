069cc76fc53115fb7b0770003112e9423e245bfa
HoaiNampro <namn2591@gmail.com>
2024-04-29 09:19:01 +0700

    add N for nvarchar in insert sql

--- database/insert_data/sale_database/Repres_table.sql
+++ database/insert_data/sale_database/Repres_table.sql
@@ -1,50 +1,50 @@
 ﻿INSERT INTO RepresentativeOffices (CityId, CityName, OfficeAddress, State, Time)
-VALUES ('CT01', 'Hà Nội', 'Địa chỉ văn phòng Hà Nội', 'Hà Nội', '2024-04-28');
+VALUES ('CT01', N'Hà Nội', N'Địa chỉ văn phòng Hà Nội', N'Miền bắc', '2024-04-28');
 
 INSERT INTO RepresentativeOffices (CityId, CityName, OfficeAddress, State, Time)
-VALUES ('CT02', 'Hồ Chí Minh', 'Địa chỉ văn phòng Hồ Chí Minh', 'Hồ Chí Minh', '2024-04-28');
+VALUES ('CT02', N'Hồ Chí Minh', N'Địa chỉ văn phòng Hồ Chí Minh', N'Miền nam', '2024-04-28');
 
 INSERT INTO RepresentativeOffices (CityId, CityName, OfficeAddress, State, Time)
-VALUES ('CT03', 'Đà Nẵng', 'Địa chỉ văn phòng Đà Nẵng', 'Đà Nẵng', '2024-04-28');
+VALUES ('CT03', N'Đà Nẵng', N'Địa chỉ văn phòng Đà Nẵng', N'Miền trung', '2024-04-28');
 
 INSERT INTO RepresentativeOffices (CityId, CityName, OfficeAddress, State, Time)
-VALUES ('CT04', 'Đà Lạt', 'Địa chỉ văn phòng Đà Lạt', 'Lâm Đồng', '2024-04-28');
+VALUES ('CT04', N'Đà Lạt', N'Địa chỉ văn phòng Đà Lạt', N'Miền trung', '2024-04-28');
 
 INSERT INTO RepresentativeOffices (CityId, CityName, OfficeAddress, State, Time)
-VALUES ('CT05', 'Nha Trang', 'Địa chỉ văn phòng Nha Trang', 'Khánh Hòa', '2024-04-28');
+VALUES ('CT05', N'Nha Trang', N'Địa chỉ văn phòng Nha Trang', N'Miền nam', '2024-04-28');
 
 INSERT INTO RepresentativeOffices (CityId, CityName, OfficeAddress, State, Time)
-VALUES ('CT06', 'Hải Phòng', 'Địa chỉ văn phòng Hải Phòng', 'Hải Phòng', '2024-04-28');
+VALUES ('CT06', N'Hải Phòng', N'Địa chỉ văn phòng Hải Phòng', N'Miền bắc', '2024-04-28');
 
 INSERT INTO RepresentativeOffices (CityId, CityName, OfficeAddress, State, Time)
-VALUES ('CT07', 'Cần Thơ', 'Địa chỉ văn phòng Cần Thơ', 'Cần Thơ', '2024-04-28');
+VALUES ('CT07', N'Cần Thơ', N'Địa chỉ văn phòng Cần Thơ', N'Miền nam', '2024-04-28');
 
 INSERT INTO RepresentativeOffices (CityId, CityName, OfficeAddress, State, Time)
-VALUES ('CT08', 'Huế', 'Địa chỉ văn phòng Huế', 'Thừa Thiên Huế', '2024-04-28');
+VALUES ('CT08', N'Huế', N'Địa chỉ văn phòng Huế', N'Miền trung', '2024-04-28');
 
 INSERT INTO RepresentativeOffices (CityId, CityName, OfficeAddress, State, Time)
-VALUES ('CT09', 'Vũng Tàu', 'Địa chỉ văn phòng Vũng Tàu', 'Bà Rịa-Vũng Tàu', '2024-04-28');
+VALUES ('CT09', N'Vũng Tàu', N'Địa chỉ văn phòng Vũng Tàu', N'Miền nam', '2024-04-28');
 
 INSERT INTO RepresentativeOffices (CityId, CityName, OfficeAddress, State, Time)
-VALUES ('CT10', 'Đồng Nai', 'Địa chỉ văn phòng Đồng Nai', 'Đồng Nai', '2024-04-28');
+VALUES ('CT10', N'Đồng Nai', N'Địa chỉ văn phòng Đồng Nai', N'Miền nam', '2024-04-28');
 
 INSERT INTO RepresentativeOffices (CityId, CityName, OfficeAddress, State, Time)
-VALUES ('CT11', 'Quảng Ninh', 'Địa chỉ văn phòng Quảng Ninh', 'Quảng Ninh', '2024-04-28');
+VALUES ('CT11', N'Quảng Ninh', N'Địa chỉ văn phòng Quảng Ninh', N'Miền bắc', '2024-04-28');
 
 INSERT INTO RepresentativeOffices (CityId, CityName, OfficeAddress, State, Time)
-VALUES ('CT12', 'Hải Dương', 'Địa chỉ văn phòng Hải Dương', 'Hải Dương', '2024-04-28');
+VALUES ('CT12', N'Hải Dương', N'Địa chỉ văn phòng Hải Dương', N'Miền bắc', '2024-04-28');
 
 INSERT INTO RepresentativeOffices (CityId, CityName, OfficeAddress, State, Time)
-VALUES ('CT13', 'Bình Dương', 'Địa chỉ văn phòng Bình Dương', 'Bình Dương', '2024-04-28');
+VALUES ('CT13', N'Bình Dương', N'Địa chỉ văn phòng Bình Dương', N'Miền nam', '2024-04-28');
 
 INSERT INTO RepresentativeOffices (CityId, CityName, OfficeAddress, State, Time)
-VALUES ('CT14', 'Long An', 'Địa chỉ văn phòng Long An', 'Long An', '2024-04-28');
+VALUES ('CT14', N'Long An', N'Địa chỉ văn phòng Long An', N'Miền nam', '2024-04-28');
 
 INSERT INTO RepresentativeOffices (CityId, CityName, OfficeAddress, State, Time)
-VALUES ('CT15', 'Hưng Yên', 'Địa chỉ văn phòng Hưng Yên', 'Hưng Yên', '2024-04-28');
+VALUES ('CT15', N'Hưng Yên', N'Địa chỉ văn phòng Hưng Yên', N'Miền bắc', '2024-04-28');
 
 INSERT INTO RepresentativeOffices (CityId, CityName, OfficeAddress, State, Time)
-VALUES ('CT16', 'Bắc Ninh', 'Địa chỉ văn phòng Bắc Ninh', 'Bắc Ninh', '2024-04-28');
+VALUES ('CT16', N'Bắc Ninh', N'Địa chỉ văn phòng Bắc Ninh', N'Miền bắc', '2024-04-28');
 
 INSERT INTO RepresentativeOffices (CityId, CityName, OfficeAddress, State, Time)
-VALUES ('CT17', 'Nam Định', 'Địa chỉ văn phòng Nam Định', 'Nam Định', '2024-04-28');
+VALUES ('CT17', N'Nam Định', N'Địa chỉ văn phòng Nam Định', N'Miền bắc', '2024-04-28');
