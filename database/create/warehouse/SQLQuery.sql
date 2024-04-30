7ac5e30a3cb2f0467624556b8b1f4840e95ad269
Phuongham1122 <128309100+Phuongham1122@users.noreply.github.com>
2024-04-30 23:14:14 +0700

    Create SQLQuery.sql

---
+++ database/create/warehouse/SQLQuery.sql
@@ -0,0 +1,97 @@
+CREATE DATABASE Warehouse
+
+CREATE TABLE DimItem (
+	ItemKey char(10) not null,
+	Descriptions nvarchar(255) not null,
+	Size nvarchar(20) not null,
+	Weight int not null,
+	Price int not null,
+	CONSTRAINT PK_Items
+	PRIMARY KEY (ItemKey)
+);
+
+CREATE TABLE DimTime (
+	TimeKey char(10) not null,
+	DayOfMonth int not null,
+	MonthOfQuater int not null,
+	QuaterOfYear int not null,
+	Year int not null
+	CONSTRAINT PK_Time
+	PRIMARY KEY (TimeKey)
+);
+
+CREATE TABLE DimRepresentativeOffice (
+	CityKey char(10) not null,
+	CityName nvarchar(50) not null,
+	OfficeAddress nvarchar(100) not null,
+	State nvarchar(100) not null,
+	Time date not null,
+	CONSTRAINT PK_City
+	PRIMARY KEY (CityKey)
+);
+
+CREATE TABLE DimStore (
+	StoreKey char (10) not null,
+	CityKey char (10) not null,
+	PhoneNumber char(12) not null,
+	CONSTRAINT PK_Store
+	PRIMARY KEY (StoreKey),
+	CONSTRAINT FK_City
+	FOREIGN KEY (CityKey)
+	REFERENCES DimRepresentativeOffice(CityKey)
+);
+
+CREATE TABLE DimCustomers (
+	CustomerKey char(10) not null,
+	FullName nvarchar(50) not null,
+	CityKey char(10) not null,
+	TravelTourGuide nvarchar(50) not null,
+	PostalAddress nvarchar(50) not null,
+	CONSTRAINT PK_Customers
+	PRIMARY KEY (CustomerKey),
+	CONSTRAINT PK_CustomerOfCity
+	FOREIGN KEY (CityKey)
+	REFERENCES DimRepresentativeOffice(CityKey)
+);
+
+CREATE TABLE FactStoreItem(
+	TimeKey char(10) not null,
+	ItemKey char(10) not null,
+	StoreKey char(10) not null,
+	QuantityOfStore int not null,
+	CONSTRAINT PK_TimeOfStoreItems
+	FOREIGN KEY (TimeKey)
+	REFERENCES DimTime(TimeKey),
+	CONSTRAINT PK_ItemsOfStore
+	FOREIGN KEY (ItemKey)
+	REFERENCES DimItem(ItemKey),
+	CONSTRAINT PK_StoreItem
+	FOREIGN KEY (StoreKey)
+	REFERENCES DimStore(StoreKey)
+);
+
+CREATE TABLE FactOrderItems (
+	TimeKey char(10) not null,
+	ItemKey char(10) not null,
+	CustomerKey char(10) not null,
+	OrderedQuantity int not null,
+	OrderedCost bigint not null,
+	Profit bigint not null,
+	CONSTRAINT PK_TimeOfOrderItems
+	FOREIGN KEY (TimeKey)
+	REFERENCES DimTime(TimeKey),
+	CONSTRAINT PK_ItemsOfOrder
+	FOREIGN KEY (ItemKey)
+	REFERENCES DimItem(ItemKey),
+	CONSTRAINT PK_OrderOfCustomer
+	FOREIGN KEY (CustomerKey)
+	REFERENCES DimCustomers(CustomerKey)
+)
+
+DROP TABLE DimItem
+DROP TABLE DimStore
+DROP TABLE DimRepresentativeOffice
+DROP TABLE DimCustomers
+DROP TABLE DimTime
+DROP TABLE FactStoreItem
+DROP TABLE FactOrderItems
