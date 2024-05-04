1213d22e3af57922cbebdedad9e0e242fdd2b1d3
Quoc <nguyeanhquoc187@gmail.com>
2024-05-04 17:56:38 +0700

    update: them cube

---
+++ cube/OrderCustomerOfStateQuarterCube.sql
@@ -0,0 +1,14 @@
+﻿--Thống kê doanh thu các bang (miền) theo ngày
+
+DROP TABLE IF EXISTS OrderCustomerOfStateQuarterCube;
+SELECT top 9999999 f.*
+INTO OrderCustomerOfStateQuarterCube
+FROM (
+	SELECT dr.State, ti.Quarter, ti.Year , sum(fa.OrderedCost) as DoanhThu, sum(fa.OrderedQuantity) as SoLuongBan, sum(fa.Profit) as LoiNhuan
+	FROM FactOrderItems fa
+	INNER JOIN DimCustomer dc ON fa.CustomerKey = dc.CustomerKey
+	INNER JOIN DimRepresentativeOffice dr ON dr.CityKey = dc.CityKey 
+	INNER JOIN DimTime ti ON ti.TimeKey = fa.TimeKey
+	GROUP BY dr.State, ti.Quarter, ti.Year
+) AS f
+ORDER BY f.Year, f.Quarter;
