45d20b99680649cc7201910f5a36c3bcc7fdd17d
Quoc <nguyeanhquoc187@gmail.com>
2024-05-07 00:18:18 +0700

    fix loi phai, gop file cube

--- cube/cube1.sql
+++ cube/cube1.sql
@@ -312,6 +312,110 @@ BEGIN
     PRINT 'Bang khong ton tai'
 END
 
+--Thống kê doanh thu các thành phố trong các quý
+
+DROP TABLE IF EXISTS OrderCustomerOfCityQuarterCube;
+SELECT top 9999999 f.*
+INTO OrderCustomerOfCityQuarterCube
+FROM (
+	SELECT dr.CityKey, dr.CityName,ti.Quarter, ti.Year, sum(fa.OrderedCost) as DoanhThu, sum(fa.OrderedQuantity) as SoLuongBan, sum(fa.Profit) as LoiNhuan
+	FROM FactOrderItems fa
+	INNER JOIN DimCustomer dc ON fa.CustomerKey = dc.CustomerKey
+	INNER JOIN DimRepresentativeOffice dr ON dr.CityKey = dc.CityKey 
+	INNER JOIN DimTime ti ON ti.TimeKey = fa.TimeKey
+	GROUP BY dr.CityKey, dr.CityName, ti.Quarter, ti.Year
+) AS f
+ORDER BY f.Year, f.Quarter;
+
+DROP TABLE IF EXISTS OrderCustomerOfCityYearCube;
+SELECT top 9999999 f.*
+INTO OrderCustomerOfCityYearCube
+FROM (
+	SELECT dr.CityKey, dr.CityName, ti.Year, sum(fa.OrderedCost) as DoanhThu, sum(fa.OrderedQuantity) as SoLuongBan, sum(fa.Profit) as LoiNhuan
+	FROM FactOrderItems fa
+	INNER JOIN DimCustomer dc ON fa.CustomerKey = dc.CustomerKey
+	INNER JOIN DimRepresentativeOffice dr ON dr.CityKey = dc.CityKey 
+	INNER JOIN DimTime ti ON ti.TimeKey = fa.TimeKey
+	GROUP BY dr.CityKey, dr.CityName, ti.Year
+) AS f
+ORDER BY f.Year;
+
+DROP TABLE IF EXISTS OrderCustomerOfStateDayCube;
+SELECT top 9999999 f.*
+INTO OrderCustomerOfStateDayCube
+FROM (
+	SELECT dr.State, ti.Day, ti.Month, ti.Year , sum(fa.OrderedCost) as DoanhThu, sum(fa.OrderedQuantity) as SoLuongBan, sum(fa.Profit) as LoiNhuan
+	FROM FactOrderItems fa
+	INNER JOIN DimCustomer dc ON fa.CustomerKey = dc.CustomerKey
+	INNER JOIN DimRepresentativeOffice dr ON dr.CityKey = dc.CityKey 
+	INNER JOIN DimTime ti ON ti.TimeKey = fa.TimeKey
+	GROUP BY dr.State, ti.Day, ti.Month, ti.Year
+) AS f
+ORDER BY f.Year, f.Month, f.Day;
+
+DROP TABLE IF EXISTS OrderCustomerOfStateMonthCube;
+SELECT top 9999999 f.*
+INTO OrderCustomerOfStateMonthCube
+FROM (
+	SELECT dr.State, ti.Month, ti.Year , sum(fa.OrderedCost) as DoanhThu, sum(fa.OrderedQuantity) as SoLuongBan, sum(fa.Profit) as LoiNhuan
+	FROM FactOrderItems fa
+	INNER JOIN DimCustomer dc ON fa.CustomerKey = dc.CustomerKey
+	INNER JOIN DimRepresentativeOffice dr ON dr.CityKey = dc.CityKey 
+	INNER JOIN DimTime ti ON ti.TimeKey = fa.TimeKey
+	GROUP BY dr.State, ti.Month, ti.Year
+) AS f
+ORDER BY f.Year, f.Month;
+
+DROP TABLE IF EXISTS OrderCustomerOfStateQuarterCube;
+SELECT top 9999999 f.*
+INTO OrderCustomerOfStateQuarterCube
+FROM (
+	SELECT dr.State, ti.Quarter, ti.Year , sum(fa.OrderedCost) as DoanhThu, sum(fa.OrderedQuantity) as SoLuongBan, sum(fa.Profit) as LoiNhuan
+	FROM FactOrderItems fa
+	INNER JOIN DimCustomer dc ON fa.CustomerKey = dc.CustomerKey
+	INNER JOIN DimRepresentativeOffice dr ON dr.CityKey = dc.CityKey 
+	INNER JOIN DimTime ti ON ti.TimeKey = fa.TimeKey
+	GROUP BY dr.State, ti.Quarter, ti.Year
+) AS f
+ORDER BY f.Year, f.Quarter;
+
+DROP TABLE IF EXISTS OrderCustomerOfStateYearCube;
+SELECT top 9999999 f.*
+INTO OrderCustomerOfStateYearCube
+FROM (
+	SELECT dr.State,ti.Year , sum(fa.OrderedCost) as DoanhThu, sum(fa.OrderedQuantity) as SoLuongBan, sum(fa.Profit) as LoiNhuan
+	FROM FactOrderItems fa
+	INNER JOIN DimCustomer dc ON fa.CustomerKey = dc.CustomerKey
+	INNER JOIN DimRepresentativeOffice dr ON dr.CityKey = dc.CityKey 
+	INNER JOIN DimTime ti ON ti.TimeKey = fa.TimeKey
+	GROUP BY dr.State, ti.Year
+) AS f
+ORDER BY f.Year;
+
+DROP TABLE IF EXISTS OrderItemsCustomerCube;
+SELECT top 9999999 f.*
+INTO OrderItemsCustomerCube
+FROM (
+	SELECT dc.CustomerKey, dc.CustomerName, di.ItemKey, di.Descriptions , sum(fa.OrderedCost) as DoanhThu, sum(fa.OrderedQuantity) as SoLuongBan, sum(fa.Profit) as LoiNhuan
+	FROM FactOrderItems fa
+	INNER JOIN DimCustomer dc ON fa.CustomerKey = dc.CustomerKey
+	INNER JOIN DimItem di ON fa.ItemKey = di.ItemKey
+	GROUP BY dc.CustomerKey, dc.CustomerName, di.ItemKey, di.Descriptions
+) AS f
+ORDER BY f.CustomerKey;
+
+DROP TABLE IF EXISTS OrderItemsCustomerOfCityCube;
+SELECT top 9999999 f.*
+INTO OrderItemsCustomerOfCityCube
+FROM (
+	SELECT dr.CityKey, dr.CityName, di.ItemKey, di.Descriptions , sum(fa.OrderedCost) as DoanhThu, sum(fa.OrderedQuantity) as SoLuongBan, sum(fa.Profit) as LoiNhuan
+	FROM FactOrderItems fa
+	INNER JOIN DimCustomer dc ON fa.CustomerKey = dc.CustomerKey
+	INNER JOIN DimRepresentativeOffice dr ON dc.CityKey = dr.CityKey
+	INNER JOIN DimItem di ON fa.ItemKey = di.ItemKey
+	GROUP BY dr.CityKey, dr.CityName, di.ItemKey, di.Descriptions
+) AS f
+ORDER BY f.CityName;
 
 
 
